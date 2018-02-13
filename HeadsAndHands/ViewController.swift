37786c95a292b5cb52d2d125c3a5c9ccae717af3
Синютин Андрей <andrew@macbook-pro-sinutin.local>
2018-02-13 08:48:58 +0300

    Initial Commit

---
+++ HeadsAndHands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeadsAndHands
+//
+//  Created by Синютин Андрей on 13.02.18.
+//  Copyright © 2018 sinyutin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
